bafe5121540182514bd4f7436d45e8301d249bc3
Changkyu Ahn <changkyu.ahn@navercorp.com>
2021-04-09 17:04:38 +0900

    HelloResponseDto 적용 및 테스트 추가

--- src/main/java/io/github/positoy/studyboot/web/HelloController.java
+++ src/main/java/io/github/positoy/studyboot/web/HelloController.java
@@ -1,14 +1,16 @@
 package io.github.positoy.studyboot.web;
 
+import io.github.positoy.studyboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("hello")
-    public String hello() {
-        return "hello";
+    public HelloResponseDto hello(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 
 }
--- src/test/java/io/github/positoy/studyboot/web/HelloControllerTest.java
+++ src/test/java/io/github/positoy/studyboot/web/HelloControllerTest.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -19,10 +20,16 @@ class HelloControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void hello_리턴된다() throws Exception {
-        String hello = "hello";
-        mockMvc.perform(get("/hello"))
+    void helloDto_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 10;
+        mockMvc.perform(
+                get("/hello")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
-                .andExpect(content().string(hello));
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
     }
 }
