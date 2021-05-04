3144cffcaeb61d1db1f31fab1a6ab2f04c7fcbbd
Changkyu Ahn <changkyu.ahn@navercorp.com>
2021-05-04 15:52:24 +0900

    글등록 구현

--- src/main/java/io/github/positoy/studyboot/web/IndexController.java
+++ src/main/java/io/github/positoy/studyboot/web/IndexController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
